b052f649f36df8b701ec4e4361dec65e1fdf40de
Victor Hugo Aguilar <37599330+VictorHugoAguilar@users.noreply.github.com>
2022-09-03 12:26:29 +0200

    create insert basic product in db

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Config prefix global route basic
+  app.setGlobalPrefix('api');
+
+  // Config global pipes
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
